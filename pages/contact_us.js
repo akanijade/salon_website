7b4d83b7c5cdf437f88647bf4e7711022698d996
Sheharyar Khan <sheharyar.alam.khan@gmail.com>
2021-07-09 22:25:57 -0700

    added navbar

---
+++ pages/contact_us.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar'
+
+export default function ContactUs() {
+  return (
+    <div className="container">
+      <Navbar page="Contact Us"></Navbar>
+      Contact Us
+    </div>
+  )
+}
