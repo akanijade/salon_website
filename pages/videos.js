25692a712a81cff9cccf95f1bac02ec0f9094b92
Tanzim A <37065007+Tanzaik@users.noreply.github.com>
2021-07-14 22:06:48 -0700

    Update videos.js

--- pages/videos.js
+++ pages/videos.js
@@ -1,5 +1,5 @@
 import Navbar from '../components/Navbar'
-import contactStyle from '../styles/variety.module.css'
+import contactStyle from '../styles/contact_us.module.css'
 
 export default function Videos() {
     const currentPage = (page) => 
@@ -24,17 +24,13 @@ return (
             </p>
         </div>
         
-    
-    </div>
-)
-
-<div className={contactStyle.image}>
+        <div className={contactStyle.image}>
 				<iframe className={contactStyle.iframe}
 					src="https://static.wixstatic.com/media/c837a6_437653b7f8dd44db89caa4bbd8f192cf~mv2.jpg/v1/fill/w_1960,h_1404,al_c,q_90,usm_0.66_1.00_0.01/c837a6_437653b7f8dd44db89caa4bbd8f192cf~mv2.webp"
 					width="980" height="702" allowfullscreen="" loading="lazy">
 				</iframe>
-		
 
-
+    </div>
+)
 
 }
